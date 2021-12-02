4ce0b0bd4b8117d5ee4d3b8f9c8ca86ab6908bfe
Arne Bahlo <arne@axiom.co>
2021-12-02 11:47:32 +0100

    Create personal token

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as http from '@actions/http-client';
+import {tokens} from '@axiomhq/axiom-node';
 
 const AxiomUrl = 'http://localhost:8080';
+const AxiomEmail = 'info@axiom.co';
+const AxiomPassword = 'setup-axiom';
 
 const sleep = (ms: number) => {
   return new Promise((resolve, _reject) => setTimeout(resolve, ms));
 };
 
 async function startStack(version: string) {
-  core.startGroup('docker compose up -d');
   await exec.exec('docker', ['compose', 'up', '-d', '--quiet-pull'], {
     env: {
       AXIOM_VERSION: version
     }
   });
-  core.endGroup();
 }
 
 async function waitUntilReady(client: http.HttpClient) {
-  core.info('Waiting until Axiom is ready');
   for (let i = 0; i < 10; i++) {
     try {
       await client.get(AxiomUrl);
@@ -31,16 +31,54 @@ async function waitUntilReady(client: http.HttpClient) {
 }
 
 async function initializeDeployment(client: http.HttpClient) {
-  core.info('Initializing deployment');
   const res = await client.postJson(`${AxiomUrl}/auth/init`, {
     org: 'setup-axiom',
     name: 'setup-axiom',
-    email: 'info@axiom.co',
-    password: 'setup-axiom'
+    email: AxiomEmail,
+    password: AxiomPassword
   });
   if (res.statusCode !== 200) {
     throw new Error(`Failed to initialize deployment`);
   }
+}
+
+async function createPersonalToken(client: http.HttpClient): Promise<string> {
+  const sessionRes = await client.post(
+    `${AxiomUrl}/auth/signin/credentials`,
+    JSON.stringify({
+      email: AxiomEmail,
+      password: AxiomPassword
+    }),
+    {
+      'Content-Type': 'application/json'
+    }
+  );
+  const cookieHeader = sessionRes.message.headers['set-cookie'];
+  if (!cookieHeader) {
+    throw new Error(
+      `Failed to get session cookie, please create an issue at <https://github.com/axiomhq/setup-axiom/issues/new>`
+    );
+  }
+  const cookie = cookieHeader[0].split(';')[0];
+
+  const tokenRes = await client.postJson<tokens.Token>(
+    `${AxiomUrl}/api/v1/tokens/personal`,
+    {
+      name: 'setup-axiom',
+      description:
+        'This token is automatically created by github.com/axiomhq/setup-axiom'
+    },
+    {cookie}
+  );
+
+  const rawToken = await client.getJson<tokens.RawToken>(
+    `${AxiomUrl}/api/v1/tokens/personal/${tokenRes.result!.id}/token`,
+    {cookie}
+  );
+
+  await client.post(`${AxiomUrl}/logout`, '', {cookie});
+
+  return rawToken.result!.token;
 }
 
 export async function run() {
@@ -48,12 +86,23 @@ export async function run() {
     let version = core.getInput('axiom-version');
     const client = new http.HttpClient('github.com/axiomhq/setup-axiom');
 
-    core.info(`Starting Axiom ${version} stack`);
+    core.info(`Setting up Axiom ${version}`);
+
+    core.startGroup('Starting stack');
     await startStack(version);
+    core.endGroup();
+
+    core.info('Waiting until Axiom is ready');
     await waitUntilReady(client);
+
+    core.info('Initializing deployment');
     await initializeDeployment(client);
 
-    core.info(`Axiom stack started and configured`);
+    core.info('Creating personal token');
+    const personalToken = await createPersonalToken(client);
+    core.setOutput('personal-token', personalToken);
+
+    core.info(`Axiom is running and configured`);
   } catch (error: any) {
     core.setFailed(error.message);
   }
