2f97b9067b9537fc4a9747d7ec6e13c2dfa1c5ec
Arne Bahlo <arne@axiom.co>
2022-06-20 14:07:56 +0200

    Expose AXIOM_DB_PORT in post step as well
    This hides the warning of missing env variables

--- src/post.ts
+++ src/post.ts
@@ -1,10 +1,14 @@
 import * as core from '@actions/core';
 import {exec} from '@actions/exec';
+import semverGte from 'semver/functions/gte';
 
 export async function run(dir: string) {
   try {
+    const version = core.getInput('axiom-version');
     const env = {
-      AXIOM_VERSION: core.getInput('axiom-version'),
+      AXIOM_VERSION: version,
+      // Starting with 1.21.0, DB exposes 8080 instead of 80
+      AXIOM_DB_PORT: semverGte(version, '1.21.0') ? '8080' : '80',
       AXIOM_PORT: core.getInput('axiom-port'),
       AXIOM_LICENSE_TOKEN: core.getInput('axiom-license'),
       AXIOM_DB_IMAGE: core.getInput('axiom-db-image'),
