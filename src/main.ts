37235046aa41bffe58b1f0dd8cc74aea2918afbf
Arne Bahlo <arne@axiom.co>
2022-06-20 13:59:01 +0200

    Reformat main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as http from '@actions/http-client';
-import { tokens } from '@axiomhq/axiom-node';
+import {tokens} from '@axiomhq/axiom-node';
 import * as io from '@actions/io';
 import * as fs from 'fs';
 import * as dockerCompose from './docker-compose';
@@ -66,15 +66,15 @@ async function createPersonalToken(
       description:
         'This token is automatically created by github.com/axiomhq/setup-axiom'
     },
-    { cookie }
+    {cookie}
   );
 
   const rawToken = await client.getJson<tokens.RawToken>(
     `${url}/api/v1/tokens/personal/${tokenRes.result!.id}/token`,
-    { cookie }
+    {cookie}
   );
 
-  await client.post(`${url}/logout`, '', { cookie });
+  await client.post(`${url}/logout`, '', {cookie});
 
   return rawToken.result!.token;
 }
@@ -94,7 +94,7 @@ export async function run(dir: string) {
       AXIOM_VERSION: version,
       AXIOM_PORT: core.getInput('axiom-port'),
       // Starting with 1.21.0, DB exposes 8080 instead of 80
-      AXIOM_DB_PORT: semverGte(version, "1.21.0") ? "8080" : "80",
+      AXIOM_DB_PORT: semverGte(version, '1.21.0') ? '8080' : '80',
       AXIOM_LICENSE_TOKEN: core.getInput('axiom-license'),
       AXIOM_DB_IMAGE: core.getInput('axiom-db-image'),
       AXIOM_CORE_IMAGE: core.getInput('axiom-core-image')
